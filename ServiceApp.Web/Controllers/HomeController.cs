a73d16f2dfcf21f1b71ddac36cd7ec58fba20741
hardik24488 <hardik.shah@srimca.edu.in>
2016-06-06 23:22:48 +0530

    AboutUs_06-06-2016

--- ServiceApp.Web/Controllers/HomeController.cs
+++ ServiceApp.Web/Controllers/HomeController.cs
@@ -17,5 +17,10 @@ namespace ServiceApp.Web.Controllers
         {
             return View();
         }
+
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
     }
 }
